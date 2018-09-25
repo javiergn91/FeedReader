69c8ccc3ca13680b7776d66c222d01c7727cf240
Milton <miltonmja@hotmail.com>
2018-09-25 17:14:49 -0300

    Bootstrap

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const ArticleController = require("./controllers/article.controller");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
 
 const { mongoose } = require("./database");
 
