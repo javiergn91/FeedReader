b6356eba4b963cde54e2ee4402a562854eb19ad7
Milton <miltonmja@hotmail.com>
2018-09-26 11:04:46 -0300

    database populate

--- database.js
+++ database.js
@@ -1,9 +1,23 @@
 const mongoose = require("mongoose");
+const ArticleController = require("./controllers/article.controller");
+const DatabasePopulation = require("./database_population");
 
-const URI = "mongodb://localhost/feed-reader-db";
+const URI = "mongodb://localhost/feed-reader-db2";
 
 mongoose.connect(URI)
-    .then(db => console.log("DB is connected"))
+    .then(db => {
+        console.log("DB is connected")
+       
+        //If no articles exists, then populate automatically the db
+        //Useful for first time initializations
+        if(ArticleController.getArticles((data) => 
+        {
+            if(data.length <= 0) {
+                console.log("DB auto population");
+                DatabasePopulation.populate();                
+            }
+        }));
+    })
     .catch(err => console.error(err));
 
 module.exports = mongoose;
